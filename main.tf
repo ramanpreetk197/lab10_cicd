cd665b26073f582fd82646ad817c7ca508a8524b
Ramanpreet Kaur <ramanpreetk197@gmail.com>
2024-08-09 23:19:11 -0400

    commit 2

--- main.tf
+++ main.tf
@@ -2,7 +2,7 @@ provider "aws"{
     region  = "us-east-1"
 }
 
-resource "aws_instance" "example"{
+resource "aws_instance" "ec2"{
 
     ami = "ami-0a0e5d9c7acc336f1"
     instance_type = "t3.micro"
