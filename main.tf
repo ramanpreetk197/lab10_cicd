78b25394209b567ae82e2abd3c39949e5f14ed1e
Ramanpreet Kaur <ramanpreetk197@gmail.com>
2024-08-10 00:07:13 -0400

    commit 5

--- main.tf
+++ main.tf
@@ -6,13 +6,8 @@ resource "aws_instance" "ec2"{
 
     ami = "ami-0a0e5d9c7acc336f1"
     instance_type = "t3.micro"
-
-    cpu_options {
-      core_count = var.core_count
-      threads_per_core = var.thread_count
-    }
     
     tags = {
-        Name = "instance_group4"
+        Name = "vm_group4"
     }
 }
