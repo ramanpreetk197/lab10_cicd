5bb6e2bd036036291eb419ae0906e55069b51e7f
Ramanpreet Kaur <ramanpreetk197@gmail.com>
2024-08-09 23:14:32 -0400

    commit 1

---
+++ main.tf
@@ -0,0 +1,18 @@
+provider "aws"{
+    region  = "us-east-1"
+}
+
+resource "aws_instance" "example"{
+
+    ami = "ami-0a0e5d9c7acc336f1"
+    instance_type = "t3.micro"
+
+    cpu_options {
+      core_count = var.core_count
+      threads_per_core = var.thread_count
+    }
+    
+    tags = {
+        Name = "instance_group4"
+    }
+}
